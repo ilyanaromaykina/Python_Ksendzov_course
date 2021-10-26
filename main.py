0a46eadc727d8c056af11f69e4ba00db687de4a9
ilyanaromaykina <ilyanaromaykina@gmail.com>
2021-10-26 20:28:18 +0300

    data structures

---
+++ main.py
@@ -0,0 +1,45 @@
+# 1
+string_type = 'words'
+# 2
+integer_type = 12
+# 3
+float_type = 12.21
+# 4
+bytes_type = bytes(2)
+# 5
+list_type = ['1200', 'hello', 77, [2, "two"]]
+# 6
+tuple_type = (5, "600")
+# 7
+mixed_set = {80, "set", ("4", 6)}
+# 8
+f = frozenset([1, 2, 3, 4, 5, 6])
+# 9
+dict_type = {"name": "Anna", "salary": 1000}
+
+# 10
+print(string_type, type(string_type))
+print(integer_type, type(integer_type))
+print(float_type, type(float_type))
+print(bytes_type, type(bytes_type))
+print(list_type, type(list_type))
+print(tuple_type, type(tuple_type))
+print(mixed_set, type(mixed_set))
+print(f, type(f))
+print(dict_type, type(dict_type))
+
+# 11
+a = "QA "
+b = "engineer"
+c = a + b
+print(c)
+
+# 12
+t = "Task"
+n = 12
+print(t, n)
+
+# 13
+group = "group"
+number = 20
+print(group + str(number))
